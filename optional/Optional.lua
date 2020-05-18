33f3e7e0a56c196e360394743745fa44ed134a13
Xpecya <31872467+Xpecya@users.noreply.github.com>
2020-05-18 22:28:35 +0800

    Create Optional.lua

---
+++ optional/Optional.lua
@@ -0,0 +1,44 @@
+-- @requirement => MetatableBuilder
+-- @author => xpecya
+Optional = {};
+
+-- create a new Optional object
+-- @param data => the data that may be null
+-- @return => Optional object
+function Optional.new(data) 
+    local result = {};
+
+    local getValue;
+    local ifPresnet;
+    local orElse;
+    if data == nil then
+        getValue = function() 
+            error("value is nil!"); 
+        end
+        ifPresnet = function() 
+            return result; 
+        end
+        orElse = function (nilHandler)
+            nilHandler();
+        end
+    else
+        getValue = function() 
+            return data; 
+        end
+        ifPresnet = function(consumer) 
+            consumer(data);
+            return result;
+        end
+        orElse = function() end
+    end
+
+    local MetatableBuilder = require "MetatableBuilder";
+    setmetatable(result, MetatableBuilder.new().immutable().index({
+        isPresent = function() 
+            return data ~= nil;
+        end, ifPresnet = ifPresnet, orElse = orElse, getValue = getValue
+    }).build());
+    return result;
+end
+
+return Optional;
