e5445e82cce2162b66ca9e56828fa400ac4f61f9
Xpecya <31872467+Xpecya@users.noreply.github.com>
2020-05-21 23:38:11 +0800

    Update Optional.lua

--- optional/Optional.lua
+++ optional/Optional.lua
@@ -1,47 +1,44 @@
 -- @requirement => MetatableBuilder
 -- @author => xpecya
-require "MetatableBuilder"
+return {
+    new = function(data) 
+        local result = {};
 
-Optional = {};
+        local getValue;
+        local ifPresnet;
+        local orElse;
+        if data == nil then
+            getValue = function() 
+                error("value is nil!"); 
+            end
+            ifPresnet = function() 
+                return result; 
+            end
+            orElse = function (nilHandler)
+                nilHandler();
+            end
+        else
+            getValue = function() 
+                return data; 
+            end
+            ifPresnet = function(consumer) 
+                consumer(data);
+                return result;
+            end
+            orElse = function() end
+        end
 
--- create a new Optional object
--- @param data => the data that may be null
--- @return => Optional object
-function Optional.new(data) 
-    local result = {};
-
-    local getValue;
-    local ifPresnet;
-    local orElse;
-    if data == nil then
-        getValue = function() 
-            error("value is nil!"); 
-        end
-        ifPresnet = function() 
-            return result; 
-        end
-        orElse = function (nilHandler)
-            nilHandler();
-        end
-    else
-        getValue = function() 
-            return data; 
-        end
-        ifPresnet = function(consumer) 
-            consumer(data);
-            return result;
-        end
-        orElse = function() end
+        local MetatableBuilder = require "MetatableBuilder";
+        setmetatable(result, MetatableBuilder.new().immutable().index({
+            isPresent = function() 
+                return data ~= nil;
+            end, 
+            ifPresnet = ifPresnet, 
+            orElse = orElse, 
+            getValue = getValue
+        }).newindex(function()
+            print("Optional object is immutable!");    
+        end).build());
+        return result;
     end
-
-    setmetatable(result, MetatableBuilder.new().immutable().index({
-        isPresent = function() 
-            return data ~= nil;
-        end, ifPresnet = ifPresnet, orElse = orElse, getValue = getValue
-    }).newindex(function()
-        print("Optional object is immutable!");    
-    end).build());
-    return result;
-end
-
-return Optional;
+}
